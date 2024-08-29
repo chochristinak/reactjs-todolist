1533507760c90332ca5a90479a4c221932e7e233
chock1991 <chochristinak@gmail.com>
2024-08-29 11:21:12 -0600

    Handle Add

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,7 @@
 import TodoCard from "./TodoCard"
 
-export default function TodoList() {
-    let todos =[
-        'Learn typescript',
-        'Study big O notation',
-        'Give more hugs'
-    ]
-
+export default function TodoList(props) {
+    const {todos} = props
         return (
          <ul className='main'>
             {todos.map((todo, todoIndex) => {
