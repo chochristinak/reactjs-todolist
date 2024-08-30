7c415530b19d7316e8434478eb9e321408a7219b
chock1991 <chochristinak@gmail.com>
2024-08-29 18:24:10 -0600

    Update edit with useEffect, persist local storage data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import TodoInput from './components/TodoInput'
 import TodoList from './components/TodoList'
 
@@ -7,11 +7,12 @@ function App(){
   const [todoValue, setTodoValue] = useState('')
 
   function persistData(newTodoList){
-    localStorage.setItem('todos', JSON.stringify({ todos: newList }))
+    localStorage.setItem('todos', JSON.stringify({ todos: newTodoList }))
   }
 
   function handleAddTodos(newTodo){
       const newTodoList = [...todos, newTodo]
+      persistData(newTodoList)
       setTodos(newTodoList)
   }
 
@@ -19,6 +20,7 @@ function App(){
     const newTodoList = todos.filter((todo, todoIndex) => {
       return todoIndex !== index
     })
+    persistData(newTodoList)
     setTodos(newTodoList)
   }
 
@@ -27,7 +29,22 @@ function App(){
     setTodoValue(valueToBeEdited)
     handleDeleteTodo(index)
   }
-  
+
+  useEffect(() => {
+    if (!localStorage) {
+      return
+    }
+
+    let localTodos = localStorage.getItem('todos')
+    if (!localTodos) {
+      return
+    }
+
+    console.log(localTodos)
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+
+  }, [])
 
   
   return (
